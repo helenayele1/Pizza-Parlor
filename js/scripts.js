d14f6b83cc473efa817b2a56b7e548b49462265c
Padma Patil <padma@email.com>
2023-07-14 20:31:13 -0700

    add pizza constructor to js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+event.preventDefault.
+}
+
+//Pizza constructor
+const Pizza = function() {
+    this.toppings = toppings
+    this.size = size 
+    this.totalPrice = price 
+};
+
+Pizza.prototype.price = function() {
+    this.totalPrice = "25$"
+}
