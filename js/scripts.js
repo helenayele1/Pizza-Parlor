6ed885909616afac8abf806c8681372736b6ef5a
Padma Patil <padma@email.com>
2023-08-01 12:55:30 -0700

    seperate BU logic from evenListener

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,41 @@
-//UI logic
+// Business Logic
+function PizzaOrder(size, topping) {
+  this.size = size;
+  this.topping = topping;
+}
 
-window.onload = function() {
-    const pizzaForm = document.getElementById("pizzaForm");
-    const totalCostElement = document.getElementById("totalCost");
-  
-    pizzaForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      const selectedTopping = document.querySelector('input[name="topping"]:checked');
-      const selectedSize = document.querySelector('select[name="size"]');
-  
-      if (selectedTopping && selectedSize) {
-        const topping = selectedTopping.value;
-        const size = selectedSize.value;
-        const pizzaOrder = new PizzaOrder(size, topping);
-  
-        totalCostElement.textContent = pizzaOrder.calculateTotalCost();
-      }
-    });
-  //BU logic
-    function PizzaOrder(size, topping) {
-      this.size = size;
-      this.topping = topping;
-    }
-  
-    PizzaOrder.prototype.calculateTotalCost = function() {
-      const sizeOptions = {
-        small: { name: "Small", price: 20 },
-        medium: { name: "Medium", price: 25 },
-        large: { name: "Large", price: 30 }
-      };
-  
-      const selectedSize = sizeOptions[this.size];
-      const totalPrice = selectedSize.price;
-      const pizzaDescription = `${selectedSize.name} pizza with ${this.topping}`;
-  
-      return `Total Cost: $${totalPrice}. ${pizzaDescription}.`;
-    };
+PizzaOrder.prototype.calculateTotalCost = function() {
+  const sizeOptions = {
+    small: { name: "Small", price: 20 },
+    medium: { name: "Medium", price: 25 },
+    large: { name: "Large", price: 30 }
   };
-  
+
+  const selectedSize = sizeOptions[this.size];
+  const totalPrice = selectedSize.price;
+  const pizzaDescription = `${selectedSize.name} pizza with ${this.topping}`;
+
+  return `Total Cost: $${totalPrice}. ${pizzaDescription}.`;
+};
+
+// UI Logic
+function handlePizzaFormSubmit(event) {
+  event.preventDefault();
+
+  const selectedTopping = document.querySelector('input[name="topping"]:checked');
+  const selectedSize = document.querySelector('select[name="size"]');
+
+  if (selectedTopping && selectedSize) {
+    const topping = selectedTopping.value;
+    const size = selectedSize.value;
+    const pizzaOrder = new PizzaOrder(size, topping);
+
+    const totalCostElement = document.getElementById("totalCost");
+    totalCostElement.textContent = pizzaOrder.calculateTotalCost();
+  }
+}
+
+window.addEventListener("load", function() {
+  const pizzaForm = document.getElementById("pizzaForm");
+  pizzaForm.addEventListener("submit", handlePizzaFormSubmit);
+});
